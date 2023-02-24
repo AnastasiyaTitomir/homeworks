b56a41c6d38c6747613f25d2955637f852ea3bb7
Anastasiya <myasnova@mail.ru>
2023-02-24 19:28:59 +0300

    final

--- homeworks/task1/Program.cs
+++ homeworks/task1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+using System;
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write(num1+" is smaller");
+}
+else
+{
+    Console.Write(num2+" is bigger");
+}
+
