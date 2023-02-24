c3443a3257fa6f0d6664be69efae5f55abcdf99f
Anastasiya <myasnova@mail.ru>
2023-02-24 20:07:09 +0300

    final

---
+++ homeworks/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход три числа и выдаёт максимальное из этих чисел
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2 & num1>num3)
+{
+    Console.Write(num1+" is biggest");
+}
+if (num2>num1 & num2>num3)
+{
+     Console.Write(num2+" is biggest");
+}
+if (num3>num1 & num3>num2)
+{
+     Console.Write(num3+" is biggest");
+}    
