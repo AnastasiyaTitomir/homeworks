b96630cfe7c3ce2f21b27d96b82975832cb97e00
Anastasiya <myasnova@mail.ru>
2023-02-25 13:27:58 +0300

    done

---
+++ homeworks/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("input number ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2==0)
+{
+    Console.Write("четное");
+}
+
---
+++ homeworks/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("input number ");
+
+Double number = Convert.ToInt32(Console.ReadLine());
+
+for (Double i = 2; i <= number; i++)
+{
+    if (i%2==0)
+    {
+        Console.Write(i);
+    }
+}
+
