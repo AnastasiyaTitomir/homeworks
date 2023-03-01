470d8a1d368c988ead326faa63f6337cfbd18165
Anastasiya <myasnova@mail.ru>
2023-03-01 17:12:54 +0300

    done

--- homeworks/task6/Program.cs
+++ homeworks/task6/Program.cs
@@ -3,15 +3,31 @@
 
 Console.Write("input number ");
 int number = Convert.ToInt32(Console.ReadLine());
-int count = 0;
 
-if (number>=-99 && number<=99)
-{
-    Console.Write("Третьей цифры нет");
-}
-else
-{
-    count = (number % 10);
-    Console.WriteLine(count);
-}
 
+// if (number>=-99 && number<=99)
+// {
+//     Console.Write("Третьей цифры нет");
+// }
+// else
+// {
+//     count = (number % 10);
+//     Console.WriteLine(count);
+// }
+
+if (number > 1000) 
+{ 
+    while(number > 999) 
+    { 
+        number /= 10; 
+    } 
+    Console.WriteLine(number % 10); 
+} else 
+if (number < 99) 
+{ 
+    Console.WriteLine("Третьей цифры в числе нет!"); 
+} 
+else 
+{ 
+    Console.WriteLine(number % 10); 
+}
---
+++ homeworks/task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели"); 
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>5 && number<8)
+{
+    Console.WriteLine("Выходной"); 
+}
+else
+{
+    Console.WriteLine("Дня с такой цифрой не существует или будний"); 
+}
