6e1ea037ccefd7a097b3426f5e4c22962706e785
Anastasiya <myasnova@mail.ru>
2023-03-01 16:43:38 +0300

    done

---
+++ homeworks/task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+Console.Write("input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (number>=-99 && number<=99)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    count = (number % 10);
+    Console.WriteLine(count);
+}
+
