cf0d115a7e08b69c57c160611bc098d1141ce76c
Anastasiya <myasnova@mail.ru>
2023-03-01 16:13:53 +0300

    done

---
+++ homeworks/task5/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+using System;
+Console.Write("input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int akk = 0;
+
+if (number>=100 && number<1000)
+{
+    count = (number % 100);
+    akk = (count / 10);
+
+    Console.WriteLine(akk);
+}
+else
+{
+    Console.Write("введите трехзначное число!");
+}
