029d16d56100aa79c08d16278211baa88aa75f3b
Anastasiya <myasnova@mail.ru>
2023-03-03 18:51:07 +0300

    done

---
+++ homeworks/task10(19)/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число: "); 
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num>9999 && num<100000)
+{
+string text = Convert.ToString(num);
+       if(text[0]==text[4] && text[1]==text[3])
+            {
+                Console.WriteLine(text + "- это палиндром");
+            }
+        else
+            {
+                Console.WriteLine(text + "- это не палиндром");
+            }
+ }
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+} 
+
+//  Console.WriteLine("Не удалось распознать число");
