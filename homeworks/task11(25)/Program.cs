f657c460230d5953958945549d41ffcfeb17d099
Anastasiya <myasnova@mail.ru>
2023-03-11 19:24:26 +0300

    done

---
+++ homeworks/task11(25)/Program.cs
@@ -0,0 +1,14 @@
+﻿// ринимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.WriteLine("введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число В");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB>0)
+    Console.WriteLine("ответ: " + Math.Pow(numA, numB));
+
+else
+    Console.WriteLine("введите натуральное число В");
