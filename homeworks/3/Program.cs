7a0958e66c87019b50298eed89018984341b04a0
Anastasiya <myasnova@mail.ru>
2023-03-15 19:35:14 +0300

    done

---
+++ homeworks/3/Program.cs
@@ -0,0 +1,13 @@
+﻿// выводит случайное число от 10 до 99 и показывет наибольшую цифру (2 семинар, 20 минута)
+
+// списала
+
+int Random = new Random().Next(10, 100);
+Console.WriteLine("случайное число от 10 до 99:  " +Random);
+
+int max = Random/10;
+int min = Random%10;
+
+    if (max>min)
+    Console.WriteLine("Наибольшая цифра: {0}", max);
+    else Console.WriteLine("Наибольшая цифра: {0}", min);
